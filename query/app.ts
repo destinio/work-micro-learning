09dee081848fa8cc0c880a6ae151e42044c0b841
Destin Lee <dleeinc@gmail.com>
2023-07-20 12:17:39 -0500

    yeah

--- query/app.ts
+++ query/app.ts
@@ -1,64 +1,75 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const PORT = 4002;
 
-const posts: any = []
-const comments: any = []
+const posts: any = [];
+const comments: any = [];
 
 enum EventType {
-  PostCreated = 'PostCreated',
-  CommentCreated = 'CommentCreated',
+  PostCreated = "PostCreated",
+  CommentCreated = "CommentCreated",
 }
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 // posts
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-app.get('/posts/:id', (req, res) => {
-  const post = posts.find((post: any) => post.id === req.params.id)
+app.get("/posts", (req, res) => {
+  const postsForResponse = posts.map((post: any) => {
+    return {
+      ...post,
+      comments: comments.filter((comment: any) => comment.postId === post.id),
+    };
+  });
 
-  res.send(post)
-})
+  res.send(postsForResponse);
+});
+app.get("/posts/:id", (req, res) => {
+  const post = posts.map((post: any) => {
+    return {
+      ...post,
+      comments: comments.filter((comment: any) => comment.postId === post.id),
+    };
+  });
+
+  res.send(post);
+});
 
 // comments
-app.get('/comments', (req, res) => {
-  res.send(comments)
-})
+app.get("/comments", (req, res) => {
+  res.send(comments);
+});
 
-app.get('/comments/:id', (req, res) => {
-  const commet = comments.find((comment: any) => comment.id === req.params.id)
-  console.log({commet})
+app.get("/comments/:id", (req, res) => {
+  const commet = comments.find((comment: any) => comment.id === req.params.id);
   if (!commet) {
-    return res.status(404).send({ msg: 'Comment not found'})
+    return res.status(404).send({ msg: "Comment not found" });
   }
-  res.send(commet)
-})
+  res.send(commet);
+});
 
 // events from event bus
-app.post('/events', (req, res) => {
-  console.log('Query Service Received Event', req.body.type)
+app.post("/events", (req, res) => {
+  console.log("Query Service Received Event", req.body.type);
 
-  const {type, data} = req.body
+  const { type, data } = req.body;
 
   if (type === EventType.PostCreated) {
-    const {id, title} = data
-    posts.push({ id, title, comments: [] })
+    const { id, title } = data;
+    posts.push({ id, title, comments: [] });
   }
 
   if (type === EventType.CommentCreated) {
-    const {id , content, postId} = data
+    const { id, content, postId } = data;
 
-    comments.push({id, content, postId})
-    console.log({comments})
+    comments.push({ id, content, postId });
+    console.log({ comments });
   }
 
-  res.send({ msg: 'OK'})
-})
+  res.send({ msg: "OK" });
+});
 
-app.listen(PORT, () => console.log(`Query service listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Query service listening on port ${PORT}`));
