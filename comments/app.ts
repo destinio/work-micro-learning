b500607169f805524bd90381c6664490a9f4851e
Destin Lee <dleeinc@gmail.com>
2023-07-20 13:22:06 -0500

    moderation basic

--- comments/app.ts
+++ comments/app.ts
@@ -1,33 +1,40 @@
-import express from 'express';
-import crypto from 'crypto';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from "express"
+import crypto from "crypto"
+import cors from "cors"
+import fetch from "node-fetch"
 
-const PORT = 4001;
+const PORT = 4001
 
-const app = express();
+enum EventTypes {
+  CommentCreated = "CommentCreated",
+  CommentModerated = "CommentModerated",
+  CommentUpdated = "CommentUpdated",
+}
+
+const app = express()
 app.use(cors())
 
-app.use(express.json());
+app.use(express.json())
 
-app.post('/posts/:id', async (req, res) => {
-  const commentId = crypto.randomUUID();
+app.post("/posts/:id", async (req, res) => {
+  const commentId = crypto.randomUUID()
 
-  const {content} = req.body
+  const { content } = req.body
 
   try {
-    await fetch('http://localhost:4005/events', {
-      method: 'POST',
+    await fetch("http://localhost:4005/events", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        type: 'CommentCreated',
+        type: EventTypes.CommentCreated,
         data: {
           id: commentId,
           content,
+          status: "pending",
           postId: req.params.id,
-        }
+        },
       }),
     })
   } catch (error) {
@@ -35,13 +42,18 @@ app.post('/posts/:id', async (req, res) => {
     throw error
   }
 
-  res.status(201).send({ id: commentId, content, postId: req.params.id})
+  res
+    .status(201)
+    .send({ id: commentId, status: "pending", content, postId: req.params.id })
 })
 
 // events from event bus
-app.post('/events', (req, res) => {
-  console.log('Comments Service Received Event', req.body.type)
+app.post("/events", (req, res) => {
+  console.log("Comments Service Received Event", req.body.type)
+
   res.send({})
 })
 
-app.listen(PORT, () => console.log(`Comments service listening on port ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Comments service listening on port ${PORT}`)
+)
